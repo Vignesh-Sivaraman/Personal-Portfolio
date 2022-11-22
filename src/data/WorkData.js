640f98284920a8778185321c79ecc09c54e92937
Sirius-18 <97686497+Vignesh-Sivaraman@users.noreply.github.com>
2022-11-22 13:58:53 +0530

    2nd project added

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,14 +1,25 @@
 import pizza_lair from "../assets/Project_Images/Pizza_Lair.png";
+import diet_hard from "../assets/Project_Images/Diet_Hard.png";
 
 export const Works = [
   {
     id: 1,
-    name: "Pizza Lair, It is build on top of React JS and Node JS",
+    name: "Pizza Lair, A mock of pizza store app",
     features:
       "It has user verification and forgot password via email, admin portal for stock management and order approval, low stock email notification and much more.... ",
     tags: ["react", "mongodb", "express js"],
     date: "05 Oct, 2022",
     imgSrc: `${pizza_lair}`,
     link: "https://viki-pizza-delivery-app.netlify.app",
+  },
+  {
+    id: 2,
+    name: "Diet Hard, A mock of diet and fitness tracking app",
+    features:
+      "It has user verification and metrics tracking features along with a decent UI/UX while following the MVC pattern for backend... ",
+    tags: ["react", "mongodb", "express js"],
+    date: "22 Nov, 2022",
+    imgSrc: `${diet_hard}`,
+    link: "https://diet-hard-app.netlify.app/",
   },
 ];
